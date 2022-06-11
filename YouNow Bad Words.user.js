274f7782bb9d898bc07ee9357572156bb41d34f2
David <59258980+zerodytrash@users.noreply.github.com>
2022-06-12 01:08:02 +0200

    Fix #hashtags and URLs not working properly

--- YouNow Bad Words.user.js
+++ YouNow Bad Words.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouNow Bad Words
 // @namespace    https://github.com/zerodytrash/YouNow-Scripts/
-// @version      0.3
+// @version      0.4
 // @description  This script allows you to write anything in YouNow chats without getting filtered
 // @author       ZerodyOne
 // @match        https://www.younow.com/*
@@ -14,7 +14,7 @@
 
     var nativeXmlHttpSend = XMLHttpRequest.prototype.send;
 
-    // Intercept POST-Requests and mondify content
+    // Intercept POST-Requests and modify comment
     XMLHttpRequest.prototype.send = function(data) {
         if(typeof data === "string" && data.indexOf("comment=") >= 0) {
             var params = new URLSearchParams(data)
@@ -31,16 +31,18 @@
     function obfuscateText(text) {
         var obfuscatedText = "";
         var magicChar = String.fromCharCode(8203); //zero-width space
-        var mention = false;
+        var enableObfuscation = true;
 
-        // Append the magic char after every second char
         for(var i = 0; i < text.length; i++) {
 
-            // Prevent from breaking @mentions
-            if(text[i] === ' ') mention = false;
-            if(text[i + 1] === '@') mention = true;
+            // A space terminates the obfuscation pause
+            if(text[i] === ' ') enableObfuscation = true;
 
-            obfuscatedText += text[i] + (i % 2 > 0 && !mention ? magicChar : "");
+            // Prevent from breaking @mentions, #hashtags and URLs
+            if(text[i + 1] === '@' || text[i + 1] === '#' || text.substring(i + 1, i + 5) === 'http') enableObfuscation = false;
+
+            // Append the magic char after every second char if obfuscation not paused
+            obfuscatedText += text[i] + (i % 2 > 0 && enableObfuscation ? magicChar : "");
         }
 
         return obfuscatedText;
