4d8f47c521dd5dcd0c0074f04d9a1ebacf15efda
David <59258980+zerodytrash@users.noreply.github.com>
2022-07-05 03:24:20 +0200

    bugfix

--- YouNow Locale Switcher.user.js
+++ YouNow Locale Switcher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouNow Locale Switcher
 // @namespace    https://zerody.one
-// @version      0.1
+// @version      0.2
 // @description  Switch the locale of the trending list and explore page (featured list) without changing your YouNow account settings
 // @author       ZerodyOne
 // @match        https://www.younow.com/*
@@ -13,43 +13,44 @@
     'use strict';
 
     var cdnHost = 'https://cdn.younow.com/';
-    var localeOverride = JSON.parse(localStorage.getItem('localeOverride'));
+    var localeOverride = localStorage.getItem('localeOverride') || null;
     var locales = ['en', 'de', 'es', 'tr', 'me', 'ww'];
     var hideOtherLocales = true;
 
     var nativeXmlHttpOpen = XMLHttpRequest.prototype.open;
     var nativeJsonParse = window.JSON.parse;
 
-    // Intercept GET-Requests and modify locale param
-    XMLHttpRequest.prototype.open = function(method, url) {
-        if(method === 'GET' && typeof url === 'string' && url.indexOf(cdnHost) === 0) {
-            if(url.includes('younow/dashboard') || url.includes('younow/trendingUsers')) {
-                let normalizedUrl = url.replace(cdnHost, '').replace(/\//g, '&');
-                let urlParams = new URLSearchParams(normalizedUrl);
-
-                if(localeOverride) {
+    if(localeOverride) {
+        // Intercept GET-Requests and modify locale param
+        XMLHttpRequest.prototype.open = function(method, url) {
+            if(method === 'GET' && typeof url === 'string' && url.indexOf(cdnHost) === 0) {
+                if(url.includes('younow/dashboard') || url.includes('younow/trendingUsers')) {
+                    let normalizedUrl = url.replace(cdnHost, '').replace(/\//g, '&');
+                    let urlParams = new URLSearchParams(normalizedUrl);
                     urlParams.set('locale', localeOverride);
                     arguments[1] = cdnHost + urlParams.toString().replace(/&/g, '/').replace(/=\//g, '/');
                 }
             }
+
+            nativeXmlHttpOpen.apply(this, arguments);
         }
 
-        nativeXmlHttpOpen.apply(this, arguments);
-    }
+        if(hideOtherLocales && localeOverride !== 'ww'){
+            window.JSON.parse = function() {
+                let data = nativeJsonParse.apply(this, arguments);
+                if(typeof data === 'object' && data !== null) {
+                    // unfortunately wtw and wtf from /reco/loggedIn doesnt have a locale flag.
+                    // so we remove the entries...
+                    if(data.wtw) data.wtw = [];
+                    if(data.wtf) data.wtf = [];
 
-    window.JSON.parse = function() {
-        let data = nativeJsonParse.apply(this, arguments);
-        if(typeof data === 'object' && data !== null && hideOtherLocales && localeOverride && localeOverride !== 'ww') {
-            // unfortunately wtw and wtf from /reco/loggedIn doesnt have a locale flag.
-            // so we remove the entries...
-            if(data.wtw) data.wtw = [];
-            if(data.wtf) data.wtf = [];
+                    // filter out trending users from other locales
+                    if(data.trending_users) data.trending_users = data.trending_users.filter(x => x.locale === localeOverride);
+                }
 
-            // filter out trending users from other locales
-            if(data.trending_users) data.trending_users = data.trending_users.filter(x => x.locale === localeOverride);
+                return data;
+            }
         }
-
-        return data;
     }
 
     let topbarFinder = setInterval(() => {
@@ -69,20 +70,18 @@
 
         [null, ...locales].forEach(locale => {
             var option = document.createElement('option');
-            option.value = locale;
-            option.innerText = locale ? locale.toUpperCase() : 'Off';
+            option.value = locale || '';
+            option.innerText = locale?.toUpperCase() || 'Off';
             option.selected = locale === localeOverride;
             select.appendChild(option);
         })
 
         select.addEventListener('change', function() {
-            localStorage.setItem('localeOverride', JSON.stringify(this.value));
+            localStorage.setItem('localeOverride', this.value);
             window.location.reload();
         });
 
         inputWrapper.appendChild(select);
         topbarRight.insertBefore(inputWrapper, topbarRight.children[1]);
-
     }, 100);
-
 })();
